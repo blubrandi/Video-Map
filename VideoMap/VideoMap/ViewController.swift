e619632d8117e3401e080ba2944a63828db7f634
Brandi <blubrandi0@gmail.com>
2020-01-16 13:22:46 -0700

    Created Xcode Project

---
+++ VideoMap/VideoMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoMap
+//
+//  Created by Brandi Bailey on 1/16/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
