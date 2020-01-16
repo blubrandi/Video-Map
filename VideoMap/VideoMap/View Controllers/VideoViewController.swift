8f290b4b0b21631213ae2ae4d50945a7bdf3469f
Brandi <blubrandi0@gmail.com>
2020-01-16 13:45:07 -0700

    Storyboard and VCs
    Added views to storyboard for map and creating a video post.  Created VCs for each view.

---
+++ VideoMap/VideoMap/View Controllers/VideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoViewController.swift
+//  VideoMap
+//
+//  Created by Brandi Bailey on 1/16/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
